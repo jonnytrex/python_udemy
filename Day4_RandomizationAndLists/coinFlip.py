e93e90f2202bc438f27f1fc2cc375fdefcb32570
Jon Trexler <albopictus@gmail.com>
2023-08-31 22:42:16 -0400

    day 4 code

---
+++ Day4_RandomizationAndLists/coinFlip.py
@@ -0,0 +1,9 @@
+import random
+
+flip = random.randint(0,1)
+
+if flip == 1:
+    print("Heads")
+else:
+     print("Tails")
+     
---
+++ Day4_RandomizationAndLists/randomNums.py
@@ -0,0 +1,10 @@
+import random
+
+random_integer = random.randint(0,5)
+print(random_integer)
+
+random_float = random.random()
+print(random_float)
+
+random_num = random_integer + random_float
+print(random_num)
